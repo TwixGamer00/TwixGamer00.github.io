8ae663692856965dd1391e78c94c5687ba8e9db6
BuildTools <ren789.nl@gmail.com>
2023-03-25 12:57:04 +0100

    Add score details

--- FrontendMentor/Results/script.js
+++ FrontendMentor/Results/script.js
@@ -1,22 +1,42 @@
 // I decided to make the scores random instead of using the JSON file.
 
 const averageElement = document.querySelector("#average");
-
-const scores = [];
 const elements = [
   document.querySelector("#reaction"),
   document.querySelector("#memory"),
   document.querySelector("#verbal"),
   document.querySelector("#visual"),
 ];
+const ratingElement = document.querySelector("#rating");
+const commentElement = document.querySelector("#comment");
 
-elements.forEach((element) => {
-  var score = Math.floor(Math.random() * 100 + 1);
-  element.textContent = score;
-  scores.push(score);
-});
+function getScores() {
+  const scores = [];
 
-var total = 0;
-scores.forEach((score) => (total += score));
+  elements.forEach((element) => {
+    var score = Math.floor(Math.random() * 100 + 1);
+    element.textContent = score;
+    scores.push(score);
+  });
 
-averageElement.textContent = Math.round(total / 4);
+  var total = 0;
+  scores.forEach((score) => (total += score));
+
+  var average = Math.round(total / 4);
+  averageElement.textContent = average;
+
+  if (average >= 70) {
+    ratingElement.textContent = "Great";
+    commentElement.textContent =
+      "You scored higher than 65% of the people who have taken these tests.";
+  } else if (average >= 40) {
+    ratingElement.textContent = "Average";
+    commentElement.textContent =
+      "You scored roughly the same as the other people who have taken these tests.";
+  } else {
+    ratingElement.textContent = "Bad";
+    commentElement.textContent =
+      "You scored lower then 70% of the other people who have taken these tests.";
+  }
+}
+getScores();
